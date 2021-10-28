a090df0b5a74dfdeb44977b2822413ada03c9d02
Scott Tolinski <scott@leveluptuts.com>
2021-10-28 15:07:08 -0600

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import SideMenu from '$lib/SideMenu.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<SideMenu nub="🚀" top="20%" theme="dark" />
